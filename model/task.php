83ebec34052eb32fdb63515988ba4237b1d3534d
petkostefan <petkostefan@gmail.com>
2021-11-08 19:28:43 +0100

    Kreiran crud za task

--- model/task.php
+++ model/task.php
@@ -14,10 +14,50 @@ class Task{
         $this->id = $id;
         $this->naslov = $naslov;
         $this->opis = $opis;
-        $this->prioritet = $prioritet
-        $this->vreme = date("d.m.y. h:m")
+        $this->prioritet = $prioritet;
+        $this->vreme = date("d.m.y. h:m");
     }
     
+
+    public static function add(Task $task, mysqli $conn){
+        $query = "INSERT INTO task(naslov, opis, prioritet, vreme, user) 
+        VALUES('$task->naslov','$task->opis','$task->prioritet','$task->vreme','$task->user')";
+        return $conn->query($query);
+    }
+
+    public static function getById($id, mysqli $conn){
+        $query = "SELECT * FROM task WHERE id=$id";
+        $myArray = array();
+        $result = $conn->query($query);
+        if($result){
+            while($row = $result->fetch_array()){
+                $myArray[] = $row;
+            }
+        }
+        return $myArray;
+    }
+
+    public static function getByUser($userid, mysqli $conn){
+        $query = "SELECT * FROM task WHERE user=$userid";
+        $myArray = array();
+        $result = $conn->query($query);
+        if($result){
+            while($row = $result->fetch_array()){
+                $myArray[] = $row;
+            }
+        }
+        return $myArray;
+    }
+
+    public function update(mysqli $conn){
+        $query = "UPDATE task SET naslov ='$this->naslov', opis ='$this->opis', prioritet ='$this->prioritet'";
+        return $conn->query($query);
+    }
+
+    public function delete(mysqli $conn){
+        $query = "DELETE FROM task WHERE id=$this->id";
+        return $conn->query($query);
+    }
 
 }
 
